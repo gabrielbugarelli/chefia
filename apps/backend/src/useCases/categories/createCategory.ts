6160f35a69a534e9f35ba22f2312c8cf98a611b3
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2023-07-01 16:58:15 -0400

    refactor: error handling in create category use case

--- apps/backend/src/useCases/categories/createCategory.ts
+++ apps/backend/src/useCases/categories/createCategory.ts
@@ -2,8 +2,14 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export const createCategory = async (request: Request, response: Response) => {
-  const { icon, name } = request.body;
+  try {
+    const { icon, name } = request.body;
 
-  const category = await Category.create({icon, name});
-  return response.send(category);
+    const category = await Category.create({icon, name});
+    return response.status(201).json(category);
+  }
+  catch(error) {
+    console.log(error);
+    response.sendStatus(500);
+  }
 };
